07b6ddba6b9059281cf4bf82350af72cbb6c8c35
DranoMax <alex.scroggins@live.com>
2014-02-25 14:40:54 +0900

    FlashCardActivity fixed
    Able to slide/flip card now.  Also refactored code, removing unnecessary
    imports and methods.

---
+++ src/com/hatstick/flashcardium/CreateDeckActivity.java
@@ -0,0 +1,41 @@
+package com.hatstick.flashcardium;
+
+import com.hatstick.flashcardium.entities.Deck;
+import com.hatstick.flashcardium.tools.DatabaseHandler;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateDeckActivity extends Activity {
+
+	private Deck deck = new Deck();
+	private DatabaseHandler db;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dialog_create_deck);
+
+		db = new DatabaseHandler(this);
+		
+		Button save = (Button)findViewById(R.id.save);
+
+		save.setOnClickListener(onSave);
+	}
+
+	private View.OnClickListener onSave=new View.OnClickListener() {
+		public void onClick(View v) {
+			EditText name=(EditText)findViewById(R.id.name);
+			EditText desc=(EditText)findViewById(R.id.decription);
+			deck.setName(name.getText().toString());
+			deck.setDescription(desc.getText().toString());
+			db.createDeck(deck);
+			Intent i = new Intent(CreateDeckActivity.this, MainActivity.class);
+			startActivity(i);
+		}
+	};
+}
---
+++ src/com/hatstick/flashcardium/entities/Card.java
@@ -0,0 +1,77 @@
+package com.hatstick.flashcardium.entities;
+
+public class Card {
+	
+	private int id;
+	private String deck;
+	private String subject;
+	private String question;
+	private String answer;
+	
+	public Card() {
+		
+	}
+	
+	public Card(String subject, String question, String answer) {
+		this.setSubject(subject);
+		this.setQuestion(question);
+		this.setAnswer(answer);
+	}
+	
+	public Card(String deck, String subject, String question, String answer) {
+		this.setDeck(deck);
+		this.setSubject(subject);
+		this.setQuestion(question);
+		this.setAnswer(answer);
+	}
+	
+	public Card(int id, String subject, String question, String answer) {
+		this.setId(id);
+		this.setSubject(subject);
+		this.setQuestion(question);
+		this.setAnswer(answer);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	public String getDeck() {
+		return deck;
+	}
+
+	public void setDeck(String deck) {
+		this.deck = deck;
+	}
+	
+	
+
+}
